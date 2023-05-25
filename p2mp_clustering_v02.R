853d5572e24999a077dde8e7c65997304788d6b4
José Alberto Hernández <josetilos@gmail.com>
2023-05-25 12:05:56 +0200

    Update p2mp_clustering_v02.R

--- p2mp_clustering_v02.R
+++ p2mp_clustering_v02.R
@@ -13,6 +13,15 @@ set.seed(1234)
 dev.off()
 
 
+# input parameters: Traffic and Distance grid
+
+# traffic peak and variability
+traffic_peak = c(10,150); traffic_var = 0.3
+max_distance = 500 #km2
+
+cat("Input: Traffic Min and Max per node (Gb/s) ", traffic_peak, " variability = ", traffic_var, "\n")
+cat("Input: dist x dist grid (Km):  ", max_distance, "\n")
+
 
 
 # TRAFFIC PROFILES
@@ -41,24 +50,14 @@ entertainment_fig_12 = c(0.504347826 ,0.433485584 ,0.260185074 ,0.167829398 ,
                          0.972822501 ,0.963979893 ,0.977263795 ,0.991648198 ,
                          0.975470586 ,0.939116517 ,0.93070927 ,0.946142154 ,
                          0.973148633 ,0.991927826 ,0.957496444 ,0.84548412)
-#all_fig_12 = [0.483478261 ,0.379098439 ,0.253187422 ,0.181990996 ,0.137334408 ,0.14945749 ,0.186133957 ,0.363498701 ,0.567904806 ,0.799219137 ,0.941691931 ,0.969132082 ,0.977054964 ,0.987005596 ,0.989352582 ,0.980225597 ,0.952913981 ,0.907681173 ,0.892724723 ,0.888548026 ,0.912223949 ,0.929476658 ,0.895311986 ,0.785927218 ,0.600556493]
-
-
-# The two baseline profiles, normalised
-v1 = c(0.179, 0.141, 0.09, 0.051, 0.038, 0.013, 0.000, 0.002, 0.256, 0.641, 
-       0.846, 0.949, 1.000, 0.923, 0.692,	0.667, 0.769, 0.667, 0.564, 0.436, 
-       0.282, 0.180, 0.154, 0.154); v1 = v1/max(v1) # business
-v2 = c(0.739, 0.251, 0.063, 0.024, 0.027, 0.026, 0.066, 0.193, 0.322, 0.383, 
-       0.330, 0.222, 0.224, 0.356, 0.480, 0.480, 0.414, 0.385, 0.420, 0.546,
-       0.694, 0.844, 1.000, 0.960); v2 = v2/max(v2) # residential
-profiles_basic = rbind(v1,v2)
-
 
 profiles = rbind(resident_fig_12, office_fig_12, 
                  transport_fig_12, entertainment_fig_12)
 profiles_weigths = c(0.2422, 0.3707, 0.4672, 0.3881); 
 profiles_weigths = profiles_weigths/sum(profiles_weigths)
 
+
+# Plotting the profiles:
 plot(c(0:23), profiles[1,], type = "b", frame = FALSE, pch = 19,
      col = "red", xlab = "Hour", ylab = "Norm. Traffic", 
      lty = 1, lwd = 1, xlim = c(0,25), ylim=c(0,1.1))
@@ -68,36 +67,39 @@ lines(c(0:23), profiles[3,], pch = 18, col = "green", type = "b",
       lty = 2, lwd = 1)
 lines(c(0:23), profiles[4,], pch = 18, col = "brown", type = "b", 
       lty = 2, lwd = 1)
-
-
 legend("topleft", legend = c("Residential", "Office",
                              "Transport","Entertainment"),
        col = c("red", "blue","green","brown"), lty = 1:2, cex = 0.68)
 
 
 
+
+
+
 # EXPERIMENT 1: ALG CA1, without GPS distance
+cat("Algorithm CA1 (No distance)\n")
 
 
 nnodes = 1000
 
-# traffic peak and variability
-traffic_peak = c(10,150); traffic_var = 0.3
 
+# mm are the traffic demands per node (in Gb/s), each one is a 1x24 vector
 mm = c()
 for (ii in c(1:nnodes)) {
   peak = runif(1,min=traffic_peak[1],max=traffic_peak[2])
   aux = abs(peak*profiles[sample(c(1:dim(profiles)[1]),size=1,prob=profiles_weigths),] + 
-    traffic_var*peak*rnorm(dim(profiles)[2]))
+              traffic_var*peak*rnorm(dim(profiles)[2]))
   mm = rbind(mm,aux)
 }
-
 rownames(mm) = paste0("hl4_",c(1:dim(mm)[1]))
 
+# mm2 are the traffic demands per node (in number of subcarriers), each one is a 1x24 vector
+mm_bloques = ceiling(mm/25)
+mm2 = mm_bloques
 
 
-
-plot(c(0:23), mm[5,], type = "b", frame = FALSE, pch = 19,
+# Plotting the daily profiles of the first 4 nodes
+plot(c(0:23), mm[1,], type = "b", frame = FALSE, pch = 19,
      col = "red", xlab = "Hour", ylab = "Traffic (Gb/s)", 
      lty = 1, lwd = 1, xlim = c(0,25), ylim=c(0,200))
 lines(c(0:23), mm[2,], pch = 18, col = "blue", type = "b", 
@@ -106,8 +108,6 @@ lines(c(0:23), mm[3,], pch = 18, col = "green", type = "b",
       lty = 2, lwd = 1)
 lines(c(0:23), mm[4,], pch = 18, col = "brown", type = "b", 
       lty = 2, lwd = 1)
-
-
 legend("topleft", legend = c("Node 1", "Node 2",
                              "Node 3","Node 4"),
        col = c("red", "blue","green","brown"), lty = 1:2, cex = 0.68)
@@ -115,37 +115,13 @@ legend("topleft", legend = c("Node 1", "Node 2",
 
 
 
-
-mm = c()
-for (ii in c(1:nnodes)) {
-  peak = runif(1,min=traffic_peak[1],max=traffic_peak[2])
-  aux = abs(peak*profiles[sample(c(1:dim(profiles)[1]),size=1),] + 
-              traffic_var*peak*rnorm(dim(profiles)[2]))
-  mm = rbind(mm,aux)
-}
-
-rownames(mm) = paste0("hl4_",c(1:dim(mm)[1]))
-
-mm_bloques = ceiling(mm/25)
-mm2 = mm_bloques
-
-
-
-max_distance = 100 #km2
+# This is for defining the locations of the nodes in a max_distance x max_distance greed
 location_mm = data.frame(x=runif(nnodes,min=0,max=max_distance),
                          y=runif(nnodes,min=0,max=max_distance))
-print("Distance Matrix ")
-# creating a distance matrix
+# creating a distance matrix from each node to each other
 geodistance = dist(location_mm, location_mm, method="euclidean")
 geodistance = geodistance/max(geodistance)
 diag(geodistance)=1
-
-
-
-mm_bloques = ceiling(mm/25)
-mm2 = mm_bloques
-
-rows.cor2 <- 0.5+0.5*cor(t(mm), use = "pairwise.complete.obs", method = "pearson")
 
 geodist2 = matrix(0,nrow=nnodes,ncol=nnodes)
 for (ii in c(1:nnodes)) {
@@ -154,25 +130,21 @@ for (ii in c(1:nnodes)) {
   }
 }
 
+
+# clustiering distance by means of traffic correlation
+rows.cor2 <- 0.5+0.5*cor(t(mm), use = "pairwise.complete.obs", method = "pearson")
 rows.cor2 = as.matrix(rows.cor2)
 geodistance = as.matrix(geodistance)
 
 
-alpha = 1 # porcentaje importancia patrones
+alpha = 1 # percentage of importance to distance and traffic correlation
 dist_matrix = alpha * rows.cor2 + (1-alpha)*geodist2
-
-#object_name <- as.dist(object_name)
-#hclust(object_name, method = "single")
-
 hclust.row <- hclust(as.dist(dist_matrix))
-
-
 
 
 # P2MP Clustering Algorithm: Hierarchical clustering of nodes
 traff_hl4_aux = rowSums(mm_bloques)
 oorder = hclust.row$order 
-#oorder = sample(c(1:nrow(mm_bloques)), size=nrow(mm_bloques), replace=F) #random asignment
 clusters_final = list()
 kk=1; winner = oorder[kk]; cc_aux = mm2[winner,]
 while (kk<=nrow(mm)) {
@@ -180,24 +152,20 @@ while (kk<=nrow(mm)) {
   for (ii in c(kk:nrow(mm))) {
     winner = oorder[ii]; 
     cc_aux = rbind(cc_aux,mm2[winner,])
-    #print(cc_aux)
     if (max(colSums(cc_aux))>16) {
       cc_aux = cc_aux[1:(dim(cc_aux)[1]-1),]
       kk =ii
       clusters_final = append(clusters_final,list(cc_aux))
       break
     }
-    if (ii==nrow(mm)) {
+    if (ii>=nrow(mm)) {
+      clusters_final = append(clusters_final,list(cc_aux))
       kk=nrow(mm)+1
       break
     }
   }
 }
-cat("1000 nodes: number of clusters CA1 = ")
-print(length(clusters_final))
-
-
-
+cat("1000 nodes: number of clusters CA1 = ", length(clusters_final), "trees\n")
 
 # Largest-First Fit Algorithm
 traff_hl4_aux = rowSums(mm_bloques)
@@ -216,20 +184,15 @@ while (kk<=nrow(mm)) {
       clusters_final = append(clusters_final,list(cc_aux))
       break
     }
-    if (ii==nrow(mm)) {
+    if (ii>=nrow(mm)) {
+      clusters_final = append(clusters_final,list(cc_aux))
       kk=nrow(mm)+1
       break
     }
   }
 }
 
-cat("1000 nodes: number of clusters LargestFit = ")
-print(length(clusters_final))
-
-
-
-
-
+cat("1000 nodes: number of clusters LargestFit = ", length(clusters_final),"\n")
 
 
 # Show a cluster
@@ -250,20 +213,19 @@ lines(c(0:23), colSums(clusters_final[[nclustershow]]),
 for (cc in c(1:nrow(clusters_final[[nclustershow]]))) {
   lines(c(0:23), clusters_final[[nclustershow]][cc,],
         pch = 18, col = "blue", type = "b", lty = 2, lwd = 1)
-  print(which(rowSums(matrix(rep(clusters_final[[nclustershow]][cc,],
-                                 dim(mm)[1]),nrow=dim(mm)[1],byrow=T)-mm_bloques)==0))
+#  print(which(rowSums(matrix(rep(clusters_final[[nclustershow]][cc,],
+#                                 dim(mm)[1]),nrow=dim(mm)[1],byrow=T)-mm_bloques)==0))
 }
 legend("topleft", legend = c("Total aggregated", "Individual"),
        col = c("red", "blue"), lty = 1:2, cex = 0.8)
 
 
-print(which(rowSums(matrix(rep(clusters_final[[nclustershow]][cc,],
-                               dim(mm)[1]),nrow=dim(mm)[1],byrow=T)-mm_bloques)==0))
 
 
 
 
 # EXPERIMENT 2: ALG CA2, including GPS distance
+cat("Algorithm CA2 (with distance)\n")
 
 nnodes = 1000
 
@@ -280,13 +242,8 @@ rownames(mm) = paste0("hl4_",c(1:dim(mm)[1]))
 mm_bloques = ceiling(mm/25)
 mm2 = mm_bloques
 
-
-
-max_distance = 500 #km2
 location_mm = data.frame(x=runif(nnodes,min=0,max=max_distance),
                          y=runif(nnodes,min=0,max=max_distance))
-print("Distance Matrix ")
-# creating a distance matrix
 geodistance = dist(location_mm, location_mm, method="euclidean")
 geodistance = geodistance/max(geodistance)
 diag(geodistance)=1
@@ -338,7 +295,8 @@ while (kk<=nrow(mm)) {
       clusters_final = append(clusters_final,list(cc_aux))
       break
     }
-    if (ii==nrow(mm)) {
+    if (ii>=nrow(mm)) {
+      clusters_final = append(clusters_final,list(cc_aux))
       kk=nrow(mm)+1
       break
     }
@@ -366,7 +324,8 @@ while (kk<=nrow(mm)) {
       clusters_final = append(clusters_final,list(cc_aux))
       break
     }
-    if (ii==nrow(mm)) {
+    if (ii>=nrow(mm)) {
+      clusters_final = append(clusters_final,list(cc_aux))
       kk=nrow(mm)+1
       break
     }
@@ -392,7 +351,8 @@ while (kk<=nrow(mm)) {
       clusters_final = append(clusters_final,list(cc_aux))
       break
     }
-    if (ii==nrow(mm)) {
+    if (ii>=nrow(mm)) {
+      clusters_final = append(clusters_final,list(cc_aux))
       kk=nrow(mm)+1
       break
     }
@@ -420,7 +380,8 @@ while (kk<=nrow(mm)) {
       clusters_final = append(clusters_final,list(cc_aux))
       break
     }
-    if (ii==nrow(mm)) {
+    if (ii>=nrow(mm)) {
+      clusters_final = append(clusters_final,list(cc_aux))
       kk=nrow(mm)+1
       break
     }
@@ -428,7 +389,7 @@ while (kk<=nrow(mm)) {
 }
 cat("Largest-First Fit Alg. Number of clusters = ", 
     length(clusters_final), "\n") 
-cat("Savings = ",(length(clusters_final)-opt_no_clusters)/length(clusters_final),"\n")
+cat("Tree Savings = ",(length(clusters_final)-opt_no_clusters)/length(clusters_final),"\n")
 
 
 
@@ -453,7 +414,8 @@ while (kk<=nrow(mm)) {
       clusters_final = append(clusters_final,list(cc_aux))
       break
     }
-    if (ii==nrow(mm)) {
+    if (ii>=nrow(mm)) {
+      clusters_final = append(clusters_final,list(cc_aux))
       kk=nrow(mm)+1
       break
     }
@@ -465,18 +427,18 @@ opt_no_clusters = length(clusters_final)
 
 
 
-# How many 100G P2MP transceivers?
-
+# How many 100G and 400G P2MP transceivers?
 tt400G = 0; tt100G = 0
 
 for (ii in c(1:length(clusters_final))) {
-  print(sum(as.numeric(apply(clusters_final[[ii]],1,max)>=4)))
   tt400G = tt400G + sum(as.numeric(apply(clusters_final[[ii]],1,max)>4))
   tt100G = tt100G + sum(as.numeric(apply(clusters_final[[ii]],1,max)<=4))
 }
-#print(tt400G)
-#print(tt100G)
+print(tt400G)
+print(tt100G)
 
+costp2mp = 12*tt400G + 6*tt100G
+cat("Cost (CU) P2MP solutions = ",costp2mp,"\n")
 
 
 # How many P2P transceivers?
@@ -502,6 +464,9 @@ cat("No. of 100G P2P transceivers", No_100G_transceivers, "\n")
 No_10G_transceivers = length(which(peak_traffic<10))
 cat("No. of 10G P2P transceivers", No_10G_transceivers, "\n") 
 
+costp2p = 2*(12*No_400G_transceivers + 8*No_200G_transceivers +
+                5*No_100G_transceivers + 1*No_10G_transceivers)
+cat("Cost (CU) Fixed P2P solutions = ",costp2p,"\n")
 
 
 
